6d8f9cdaec7680e2b5a74cdbeee6cc0157df2d60
diego-nascimento <diegostealth3@gmail.com>
2020-07-09 14:02:31 -0300

    gA e varias mudanças para envio de mail

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -32,4 +32,11 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
+new SimpleForm({
+    form: ".formphp", // seletor do formulário
+    button: "#enviar", // seletor do botão
+    erro: "<div id='form-erro'><p>Um erro ocorreu, tente para o email arianemiranda58@gmail.com</p></div>", // mensagem de erro
+    sucesso: "<div id='form-sucesso'><h2>Formulário enviado com sucesso</h2><p>Em breve eu entro em contato com você.</p></div>", // mensagem de sucesso
+  });
+
 
